f29f85bb03561f176167b741ab62279e0a96a522
bovanlaarhoven <bo.van.laarhoven@gmail.com>
2024-05-12 01:29:48 +0200

    yuh

--- src/main/java/modules/misc/ChestStealer.java
+++ src/main/java/modules/misc/ChestStealer.java
@@ -1,4 +1,11 @@
 package modules.misc;
 
 public class ChestStealer {
+    public static void toggle(boolean state) {
+        if (state) {
+            System.out.println("Stealing from chest...");
+        } else {
+            System.out.println("Not stealing from chest...");
+        }
+    }
 }
