22cd9072ebe41217e65bfe1293ae4d033091fa69
bovanlaarhoven <bo.van.laarhoven@gmail.com>
2024-05-12 00:58:16 +0200

    - removed some useless code in autosprint.java

--- src/main/java/modules/movement/AutoSprint.java
+++ src/main/java/modules/movement/AutoSprint.java
@@ -4,13 +4,4 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 
 public class AutoSprint {
-    private final MinecraftClient client = MinecraftClient.getInstance();
-
-    public void onTick() {
-        if (client.player == null) return;
-
-        if (client.options.keyForward.isPressed()) {
-            client.player.setSprinting(true);
-        }
-    }
 }
