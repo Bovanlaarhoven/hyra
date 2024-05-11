86677a9d461ddd4c8ca401d1affc46d05deb589a
bovanlaarhoven <bo.van.laarhoven@gmail.com>
2024-05-12 00:40:55 +0200

    Initial commit

---
+++ src/main/java/org/bovl/hyra/Hyra.java
@@ -0,0 +1,10 @@
+package org.bovl.hyra;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Hyra implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // Initialization code goes here
+    }
+}
---
+++ src/main/java/org/bovl/hyra/client/HyraClient.java
@@ -0,0 +1,12 @@
+package org.bovl.hyra.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class HyraClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
